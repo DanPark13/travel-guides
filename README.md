54ede2a33379028f880eb9d9c0cce099872b96ae
DanPark13 <dpark0703@gmail.com>
2023-09-16 16:21:10 -0400

    Basic Empty Calendar

---
+++ README.md
@@ -0,0 +1,5 @@
+# Travel Guides
+
+A list of travel guides around the world. Never backed by sponsors.
+
+Created with React
---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import './App.css'
+import Calendar from './components/Calendar';
+
+const App = () => {
+  return (
+    <div className="App">
+      <h1>7 Day London Itinerary</h1>
+      <p>The Perfect 7-Day Itinerary to Get an Enriching London Experience</p>
+      <Calendar />
+    </div>
+  )
+}
+
+export default App;
